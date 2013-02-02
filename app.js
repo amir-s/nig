2bba94ac1a0b9fbf261985d13c1ff62711bce446
Amir <amir.s@acm.org>
2013-02-02 23:39:17 +0330

    fixed issues, changed app

--- app.js
+++ app.js
@@ -2,34 +2,7 @@ var ni = require('nodeigniter'),
 	connect = require('connect');
 
 ni.config('app_dir', __dirname + '/app/');
-ni._initialize(function(){
-	connect.createServer(
-		connect.favicon('public/favicon.ico'),
-		connect.static('public'),
-		connect.bodyParser(),
-		connect.query(),
-		connect.cookieParser('ni_cookie'),
-		connect.session({secret: 'kM43QtvEhmhH2KK9sJac',cookie: {maxAge: 36000}}),
-		ni.router,
-		ni.not_found
-	).listen(8082);
-	console.log('Application Started on Port: '+8082);
-});
+ni.listen(8082);
 
-/*
-var Db = require('mongodb').Db;
-var Connection = require('mongodb').Connection;
-var Server = require('mongodb').Server;
-var BSON = require('mongodb').BSON;
-var ObjectID = require('mongodb').ObjectID;
 
-var db = new Db('test', new Server('localhost', 27017, {auto_reconnect: true}, {}));
-db.open(function(){});
-db.collection('articles', function(error, d) {
-	if( error ) console.log(error);
-	else console.log("\n\n\n");
-	console.log(d.findOne({name: 'AMIR'}));
-});
 
-*/
-// Retrieve
